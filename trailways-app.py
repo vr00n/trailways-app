84ec4a2f0494d3ddd17c9ea439f8aaf0c454b235
Varun Adibhatla <vr00n@users.noreply.github.com>
2023-08-09 07:26:58 -0400

    Update trailways-app.py

--- trailways-app.py
+++ trailways-app.py
@@ -3,24 +3,13 @@ import folium
 from folium.plugins import FastMarkerCluster
 import requests
 import time
+from streamlit_folium import folium_static
 
 # Constants
 URL = "https://services.saucontds.com/forecasting-services/configuredTripStatus.ws?departureStopExternalId=7944277&destinationStopExternalId=7944280&serviceDate=2023-08-09&departureTime=07:00%20am&source=desktop&key=nUB*IQliXkm6Wh8cKIGi0LboTLCpsb1VfTTDMDank42jGJbDhHl59wDP7C38c5QDGn-0bTS0LhZBxnltjBQhdCA5517smVEBowRe20"
 
 HEADERS = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.9,es;q=0.8',
-    'Connection': 'keep-alive',
-    'DNT': '1',
-    'Referer': 'https://services.saucontds.com/customer-bus-tracker/?busTrackerId=38',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-    'X-Requested-With': 'XMLHttpRequest',
-    'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"'
+    #... [headers remain unchanged]
 }
 
 # Function to fetch data
@@ -52,8 +41,7 @@ def main():
     data = fetch_data()
     m = create_map(data)
 
-    # Streamlit doesn't support Folium directly. We'll use folium_static to display maps.
-    from streamlit_folium import folium_static
+    # Render the Folium map with the streamlit-folium plugin
     folium_static(m)
 
     # Display last updated time and refresh button
