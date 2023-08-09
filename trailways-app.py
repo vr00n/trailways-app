18a8626e688889c61710e975adf077c382b478ef
Varun Adibhatla <vr00n@users.noreply.github.com>
2023-08-09 07:34:09 -0400

    Update trailways-app.py

--- trailways-app.py
+++ trailways-app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from folium.plugins import FastMarkerCluster
 import requests
 import time
-from streamlit_folium import folium_static
+import pandas as pd
 
 # Constants
 URL = "https://services.saucontds.com/forecasting-services/configuredTripStatus.ws?departureStopExternalId=7944277&destinationStopExternalId=7944280&serviceDate=2023-08-09&departureTime=07:00%20am&source=desktop&key=nUB*IQliXkm6Wh8cKIGi0LboTLCpsb1VfTTDMDank42jGJbDhHl59wDP7C38c5QDGn-0bTS0LhZBxnltjBQhdCA5517smVEBowRe20"
@@ -16,32 +15,31 @@ def fetch_data():
     response = requests.get(URL, headers=HEADERS)
     return response.json()
 
-# Function to create and display map
-def create_map(data):
-    m = folium.Map(location=[data['coachPosition']['latitude'], data['coachPosition']['longitude']], zoom_start=13)
+# Display map using st.map()
+def display_map(data):
+    # Extracting the vehicle's current position
+    current_position = {
+        "latitude": data['coachPosition']['latitude'],
+        "longitude": data['coachPosition']['longitude']
+    }
     
-    # Add vehicle position
-    folium.Marker(
-        [data['coachPosition']['latitude'], data['coachPosition']['longitude']],
-        popup=f"Bus: {data['coachPosition']['name']}<br>Location: {data['coachPosition']['streetLocation']}",
-        icon=folium.Icon(color='red', icon='bus')
-    ).add_to(m)
+    # Extracting the vehicle's path
+    path_data = data['coachPosition']['vehiclePath']
+    df_path = pd.DataFrame(path_data)
+    
+    # Rendering the map
+    map_data = pd.DataFrame([current_position])
+    st.map(map_data)
 
-    # Add path
-    vehicle_path = [(point['lat'], point['lng']) for point in data['coachPosition']['vehiclePath']]
-    folium.PolyLine(vehicle_path, color="blue", weight=2.5, opacity=1).add_to(m)
-
-    return m
+    # If you'd also like to display the path, you can do:
+    st.map(df_path)
 
 # Main Streamlit app
 def main():
     st.title("Bus Tracker")
 
     data = fetch_data()
-    m = create_map(data)
-
-    # Render the Folium map with the streamlit-folium plugin
-    folium_static(m)
+    display_map(data)
 
     # Display last updated time and refresh button
     st.write(f"Last updated at: {time.strftime('%H:%M:%S')}")
